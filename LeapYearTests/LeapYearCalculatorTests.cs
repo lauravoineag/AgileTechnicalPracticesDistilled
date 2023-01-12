8d80099acbc6c6d11665ff629af0041208246d9a
Laura <lauravoineag87@gmail.com>
2023-01-12 19:13:47 +0000

    Add tests for years divisible or not by 4

--- LeapYearTests/LeapYearCalculatorTests.cs
+++ LeapYearTests/LeapYearCalculatorTests.cs
@@ -31,6 +31,32 @@ public class Tests
         var actual = leapYearCalc.IsLeapYear(2024);
         //Assert
         Assert.That(actual, Is.EqualTo(true));
-    } 
+    }
+
+    [TestCase(2000)]
+    [TestCase(2004)]
+    [TestCase(2008)]
+    public void when_I_pass_in_a_year_divisible_4_then_I_will_get_true(int year)
+    {
+        //Arrange
+        var leapYearCalc = new LeapYearCalc();
+        //Act
+        var actual = leapYearCalc.IsLeapYear(year);
+        //Assert
+        Assert.That(actual, Is.EqualTo(true));
+    }
+    
+    [TestCase(2001)]
+    [TestCase(2002)]
+    [TestCase(2003)]
+    public void when_I_pass_in_a_year_not_divisible_by_4_then_I_will_get_false(int year)
+    {
+        //Arrange
+        var leapYearCalc = new LeapYearCalc();
+        //Act
+        var actual = leapYearCalc.IsLeapYear(year);
+        //Assert
+        Assert.That(actual, Is.EqualTo(false));
+    }
 }
     
